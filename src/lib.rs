fbb8c182f9e6091a1341589c3311fc16dcfd1b24
Newton Ni <nwtnni@gmail.com>
2022-02-12 09:15:43 -0500

    Remove some allocations
    Introducing constants for probability reduces runtime from about 10.5s
    down to 9s.

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fmt;
 
 use num_bigint::BigInt;
 use num_rational::BigRational;
+use once_cell::sync::Lazy;
 
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq, Hash)]
 pub struct Stone {
@@ -60,6 +61,16 @@ pub enum Chance {
     P75,
 }
 
+static P25: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(25), BigInt::from(100)));
+static P35: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(35), BigInt::from(100)));
+static P45: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(45), BigInt::from(100)));
+static P55: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(55), BigInt::from(100)));
+static P65: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(65), BigInt::from(100)));
+static P75: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(75), BigInt::from(100)));
+
+static ONE: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(1), BigInt::from(1)));
+static ZERO: Lazy<BigRational> = Lazy::new(|| BigRational::new(BigInt::from(0), BigInt::from(1)));
+
 impl fmt::Display for Chance {
     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
         let chance = match self {
@@ -104,30 +115,26 @@ impl Chance {
         }
     }
 
-    pub fn to_probability_success(&self) -> BigRational {
-        let numerator = match self {
-            Chance::P25 => 25,
-            Chance::P35 => 35,
-            Chance::P45 => 45,
-            Chance::P55 => 55,
-            Chance::P65 => 65,
-            Chance::P75 => 75,
-        };
-
-        BigRational::new(BigInt::from(numerator), BigInt::from(100))
+    pub fn to_probability_success(&self) -> &'static BigRational {
+        match self {
+            Chance::P25 => &*P25,
+            Chance::P35 => &*P35,
+            Chance::P45 => &*P45,
+            Chance::P55 => &*P55,
+            Chance::P65 => &*P65,
+            Chance::P75 => &*P75,
+        }
     }
 
-    pub fn to_probability_failure(&self) -> BigRational {
-        let numerator = match self {
-            Chance::P25 => 75,
-            Chance::P35 => 65,
-            Chance::P45 => 55,
-            Chance::P55 => 45,
-            Chance::P65 => 35,
-            Chance::P75 => 25,
-        };
-
-        BigRational::new(BigInt::from(numerator), BigInt::from(100))
+    pub fn to_probability_failure(&self) -> &'static BigRational {
+        match self {
+            Chance::P25 => &*P75,
+            Chance::P35 => &*P65,
+            Chance::P45 => &*P55,
+            Chance::P55 => &*P45,
+            Chance::P65 => &*P35,
+            Chance::P75 => &*P25,
+        }
     }
 }
 
@@ -151,7 +158,7 @@ const BAD: u8 = 4;
 
 pub fn evaluate(stone: Stone) -> (usize, BigRational) {
     let mut max_line = 0;
-    let mut max_value = BigRational::new(BigInt::from(0u8), BigInt::from(1u8));
+    let mut max_value = ZERO.clone();
     let mut cache = HashMap::new();
 
     for line in 0..3 {
@@ -162,7 +169,7 @@ pub fn evaluate(stone: Stone) -> (usize, BigRational) {
         }
     }
 
-    (max_line, max_value)
+    (max_line, max_value.clone())
 }
 
 fn recurse_weighted(
@@ -170,8 +177,8 @@ fn recurse_weighted(
     cache: &mut HashMap<Stone, BigRational>,
     line: usize,
 ) -> BigRational {
-    stone.chance.to_probability_success() * recurse(stone.succeed(line), cache)
-        + stone.chance.to_probability_failure() * recurse(stone.fail(line), cache)
+    recurse(stone.succeed(line), cache) * stone.chance.to_probability_success()
+        + recurse(stone.fail(line), cache) * stone.chance.to_probability_failure()
 }
 
 fn recurse(stone: Stone, cache: &mut HashMap<Stone, BigRational>) -> BigRational {
@@ -179,15 +186,13 @@ fn recurse(stone: Stone, cache: &mut HashMap<Stone, BigRational>) -> BigRational
         return value.clone();
     }
 
-    dbg!(cache.len());
-
     // Terminal state: success
     if stone.lines[0] >= GOOD
         && stone.lines[1] >= GOOD
         && stone.rolls[2] == ROLLS
         && stone.lines[2] <= BAD
     {
-        return BigRational::new(BigInt::from(1i8), BigInt::from(1i8));
+        return ONE.clone();
     }
 
     // Terminal state: failure
@@ -195,14 +200,14 @@ fn recurse(stone: Stone, cache: &mut HashMap<Stone, BigRational>) -> BigRational
         || ROLLS - stone.rolls[1] + stone.lines[1] < GOOD
         || stone.lines[2] > BAD
     {
-        return BigRational::new(BigInt::from(0u8), BigInt::from(1u8));
+        return ZERO.clone();
     }
 
     let max = (0..3)
         .filter(|line| stone.rolls[*line] < ROLLS)
         .map(|line| recurse_weighted(stone, cache, line))
         .max()
-        .unwrap_or_else(|| BigRational::new(BigInt::from(0u8), BigInt::from(1u8)));
+        .unwrap_or_else(|| ZERO.clone());
 
     cache.insert(stone, max.clone());
     max
